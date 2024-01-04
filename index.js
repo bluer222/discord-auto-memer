0652110bf86285fd8ca185aefa0d5cb0ea1986c0
Samuel Merlin <57820015+supremegme@users.noreply.github.com>
2024-01-04 17:18:11 -0500

    comments+update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//import discord api
 const { Client, Events, GatewayIntentBits } = require("discord.js");
-const https = require("https");
+//import file stuff to load dictionary
 const fs = require("fs");
+//discord permissions or somthing
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,27 +12,44 @@ const client = new Client({
   ],
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
+//get the dictionary from words.txt and put in dictionary, same with companies
 console.log("getting dictionary");
 var dictionary = fs.readFileSync("./words.txt", "utf8").split("\n");
+var companies = fs.readFileSync("./companies.txt", "utf8").split("\n");
 console.log(dictionary);
-
+//when new message
 client.on("messageCreate", (message) => {
   // Check if the message author is not the bot itself
   if (message.author.bot) return;
+  //log the message
   console.log("Message received:", message.content);
-  var words = message.content.split(" ");
+  //remove periods and stuff
+  const cleanedmessage = message.content.replace(/[,.!?;:]/g, "");  // Replace specified punctuation
+  //split into words
+  var words = cleanedmessage.content.split(" ");
+  //create a list of search terms for later
   var search = ["meme"];
+  //cycle through words
   words.forEach(function (word, index) {
+    //if the word is a name(not normal english) then add it to the search terms along with the words next to it
     if (dictionary.includes(word.toLowerCase())) {
     } else {
       search.push(words[index]);
       search.push(words[index - 1]);
       search.push(words[index + 1]);
     }
+    //if the word is a company then add it back
+    if (companies.includes(word.toLowerCase())) {
+      search.push(words[index]);
+    }
   });
+  //remove any undefined value(if the first word is a name then it will add undefined)
   search = search.filter((value) => value !== undefined);
+  //log the search its going to do
   console.log(search);
+  //if there are any search terms other than mem
   if (search.length > 1) {
+    //use google image api to get memes
     fetch(
       "https://www.googleapis.com/customsearch/v1?q=" +
         search +
@@ -40,7 +59,8 @@ client.on("messageCreate", (message) => {
     )
       .then((response) => response.json()) // Parse JSON when response arrives
       .then((data) => {
-        const results = data.items; // Log the parsed data\
+        const results = data.items;
+        //for each meam send the meme
         results.forEach(function (result, index) {
             if(index < 3){
           message.channel.send(result.link);
