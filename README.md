fa55c7fe41a5978bfb9b2330f0116f5acc19663e
Samuel Merlin <57820015+supremegme@users.noreply.github.com>
2024-01-08 17:27:33 -0500

    tons of improvements

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # discord auto memer
  automatically finds relevant memes and posts them     
 Goals    
-1. identify memable messages and create search term
-    1. remove any words from word list leaving only names(keep the words around the name for context)
-    2. identify any company names/products and add them back if removed
-    3. if there are any words remaining then proceed
+1. identify memable messages and create search term, this has two options, ai and no ai, in index.js you can choose openai, eden ai(https://www.edenai.co/) or set both to false and use the non ai
+    1. ai, in this it uses an ai to convert messages into search temrms for memes
+    2. the following is how it does without ai
+        1. remove any words from word list leaving only names(keep the words around the name for context)
+        2. identify any company names/products and add them back if removed
+        3. if there are any words remaining then proceed
 2. search for memes
     1. use google api to get images
     2. check for repeats(not implemented)
     3. ensure the meme actually contains the name(not implemented)
 3. send top 3 memes
+
+This bot requires a couple env api keys     
+1. DISCORD_TOKEN, this is your discord bot token
+2. GOOGLE_API, this is your google image search api key
+3. OPENAI_API_KEY, this is only required if your using openai
+3. EDEN_AI, this is only required if your using eden ai
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-//import discord api
+//ai is reccomended because without it this is bad, only use one of them
+const useopenAi = false; //put key in OPENAI_API_KEY
+const useedenAi = true; //put key in EDEN_AI
+//import discord api put key in DISCORD_TOKEN
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 //import file stuff to load dictionary
+var sdk;
+var chatgpt;
+var openai;
+var aiStartPrompt;
+var dictionary;
+var companies;
 const fs = require("fs");
+if(useedenAi){
+//import eden.ai api
+ sdk = require('api')('@eden-ai/v2.0#1e32zlqwkiuup');
+}
+if(useopenAi){
+//import openai api
+ openai = require("openai");
+ chatgpt = new openai();
+}
 //discord permissions or somthing
 const client = new Client({
   intents: [
@@ -12,62 +30,126 @@ const client = new Client({
   ],
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
-//get the dictionary from words.txt and put in dictionary, same with companies
-console.log("getting dictionary");
-var dictionary = fs.readFileSync("./words.txt", "utf8").split("\n");
-var companies = fs.readFileSync("./companies.txt", "utf8").split("\n");
-console.log(dictionary);
-//when new message
+//if there is no ai then load dictionaries
+if (!useopenAi && !useedenAi) {
+  console.log("getting dictionary");
+   dictionary = fs.readFileSync("./words.txt", "utf8").split("\n");
+   companies = fs.readFileSync("./companies.txt", "utf8").split("\n");
+  console.log(dictionary);
+}else{
+//if there is ai load start prompt
+ aiStartPrompt = "I want you to do a simple task for me. You will be a meme search term bot creating search terms for memes on the internet. Simply take any input provided, find the key noun and verb, figure out what the input feels about that keyword (ONLY good or bad). If it is too simple, neutral, and not memeable, such as 'my name is jerry', respond with 'non memable'. If it is memable, respond with ONLY the keyword followed by the feeling meme, for example 'windows is bad meme'. Do NOT include anything else like dashes, quotes, bullet points, etc, you can only have words and spaces.";
+}
+//function to convert messages to search terms using ai or algorithem
+async function convertToSearchTerms(message) {
+  if (useedenAi) {
+      await sdk.auth('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODYwMjc2OGMtY2M3Yy00NzViLTgwYTQtNDRmOWUyM2VjNTQyIiwidHlwZSI6ImFwaV90b2tlbiJ9.qf2bDwM0v_pBNUAcz4rNdPTH6mxnp48dszQfa5ZhW5g');
+      let data = await sdk.text_chat_create({
+        response_as_dict: true,
+        attributes_as_list: false,
+        show_original_response: false,
+        temperature: 0,
+        max_tokens: 500,
+        providers: 'google',
+        chatbot_global_action: aiStartPrompt,
+        text: "remember, you are a search term bot, only respond with search terms seperated by spaces, if it is not memable then respond with \'non memable\', here is the message: '" + message.content + "'",
+      }).then((data) => {
+              searchGoogle(data.data.google.generated_text, message);
+        });
+  } else if (useopenAi) {
+    //if ai then query the openai api with a prompt and get the responce
+    chatgpt.chat.completions
+      .create({
+        messages: [
+          {
+            role: "system",
+            content: aiStartPrompt,
+          },
+          {
+            role: "user",
+            content:
+              "remember, you are a search term bot, only respond with search terms seperated by spaces, if it is not memable then respond with 'non memable', here is the message: 'why are you using windows 10, linux is so much better'",
+          },
+          { role: "assistant", content: "windows 10 bad meme" },
+          {
+            role: "user",
+            content:
+              "remember, you are a search term bot, only respond with search terms seperated by spaces, if it is not memable then respond with 'non memable', here is the message: '" +
+              message.content +
+              "'",
+          },
+        ],
+        model: "gpt-3.5-turbo",
+      })
+      .then((response) => response.json())
+      .then((data) => {
+            searchGoogle( data.choices.message.content, message);
+      });
+  } else {
+    //remove periods and stuff
+    const cleanedmessage = message.content.replace(/[,.!?;:]/g, ""); // Replace specified punctuation
+    //split into words
+    var words = cleanedmessage.content.split(" ");
+    //create a list of search terms for later
+    var search = ["meme"];
+    //cycle through words
+    words.forEach(function (word, index) {
+      //if the word is a name(not normal english) then add it to the search terms along with the words next to it
+      if (dictionary.includes(word.toLowerCase())) {
+      } else {
+        search.push(words[index]);
+        search.push(words[index - 1]);
+        search.push(words[index + 1]);
+      }
+      //if the word is a company then add it back
+      if (companies.includes(word.toLowerCase())) {
+        search.push(words[index]);
+      }
+    });
+    //remove any undefined value(if the first word is a name then it will add undefined)
+    search = search.filter((value) => value !== undefined);
+    if ((search.length = 1)) {
+      let results = "non memable";
+    } else {
+      let results = search;
+    }
+      searchGoogle(results, message);
+  }
+}
+function searchGoogle(search, message){
+     //log the search its going to do
+      console.log("doing search:");
+        console.log(search);
+      //make sure there is a search
+      if (!search.includes("non memable")) {
+        //use google image api to get memes
+        fetch(
+          "https://www.googleapis.com/customsearch/v1?q=" +
+            search +
+            "&cx=f5fde0317a3754392&key=" +
+            process.env.googleapi +
+            "&searchType=image",
+        )
+          .then((response) => response.json()) // Parse JSON when response arrives
+          .then((data) => {
+            const results = data.items;
+            //for each meam send the meme
+            results.forEach(function (result, index) {
+              if (index < 3) {
+                message.channel.send(result.link);
+              }
+            });
+          });
+      }
+    
+}
 client.on("messageCreate", (message) => {
   // Check if the message author is not the bot itself
   if (message.author.bot) return;
   //log the message
   console.log("Message received:", message.content);
-  //remove periods and stuff
-  const cleanedmessage = message.content.replace(/[,.!?;:]/g, "");  // Replace specified punctuation
-  //split into words
-  var words = cleanedmessage.content.split(" ");
-  //create a list of search terms for later
-  var search = ["meme"];
-  //cycle through words
-  words.forEach(function (word, index) {
-    //if the word is a name(not normal english) then add it to the search terms along with the words next to it
-    if (dictionary.includes(word.toLowerCase())) {
-    } else {
-      search.push(words[index]);
-      search.push(words[index - 1]);
-      search.push(words[index + 1]);
-    }
-    //if the word is a company then add it back
-    if (companies.includes(word.toLowerCase())) {
-      search.push(words[index]);
-    }
-  });
-  //remove any undefined value(if the first word is a name then it will add undefined)
-  search = search.filter((value) => value !== undefined);
-  //log the search its going to do
-  console.log(search);
-  //if there are any search terms other than mem
-  if (search.length > 1) {
-    //use google image api to get memes
-    fetch(
-      "https://www.googleapis.com/customsearch/v1?q=" +
-        search +
-        "&cx=f5fde0317a3754392&key=" +
-        process.env.googleapi +
-        "&searchType=image",
-    )
-      .then((response) => response.json()) // Parse JSON when response arrives
-      .then((data) => {
-        const results = data.items;
-        //for each meam send the meme
-        results.forEach(function (result, index) {
-            if(index < 3){
-          message.channel.send(result.link);
-            }
-        });
-      });
-  }
+  //convert message to meme search terms
+  let search = convertToSearchTerms(message);
 });
 
 // Log in to Discord with your client's token
